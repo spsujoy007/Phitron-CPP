44b178762e5447ff7eaae26aa36ab268d2d6cfe1
Sujoy Paul <freelancersujoy@gmail.com>
2024-01-12 00:19:45 +0600

    convert array to bst

---
+++ Semister 2/Data Stucture/Module 21 BST Binary Search Tree/convert_array_to_bst.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *convert_BST(int a[], int n, int l, int r)
+{
+    if (l > r)
+        return NULL;
+    int mid = (l + r) / 2;
+    Node *root = new Node(a[mid]);
+    Node *left_root = convert_BST(a, n, l, mid - 1);
+    Node *right_root = convert_BST(a, n, mid + 1, r);
+    root->left = left_root;
+    root->right = right_root;
+
+    return root;
+}
+
+void level_order(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    Node *root = convert_BST(a, n, 0, n - 1);
+    level_order(root);
+    return 0;
+}
