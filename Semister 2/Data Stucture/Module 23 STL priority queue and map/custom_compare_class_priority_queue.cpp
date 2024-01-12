97554b8f4a94372ea2e5e1bfb210f2d6824ceaa9
Sujoy Paul <freelancersujoy@gmail.com>
2024-01-12 17:46:23 +0600

    custom compare class priority queue

--- Semister 2/Data Stucture/Module 23 STL priority queue and map/custom_compare_class_priority_queue.cpp
+++ Semister 2/Data Stucture/Module 23 STL priority queue and map/custom_compare_class_priority_queue.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Students
+class Student
 {
 public:
     string name;
     int roll;
     int mark;
-    Students(string name, int roll, int mark)
+    Student(string name, int roll, int mark)
     {
         this->name = name;
         this->roll = roll;
@@ -15,17 +15,39 @@ public:
     }
 };
 
+class cmp
+{
+public:
+    bool operator()(Student a, Student b)
+    {
+        if (a.mark < b.mark)
+            return true;
+        else if (a.roll > b.roll)
+            return true;
+        else
+            return false;
+    }
+};
+
 int main()
 {
     int n;
     cin >> n;
+    priority_queue<Student, vector<Student>, cmp> pq;
     for (int i = 0; i < n; i++)
     {
         string name;
         int r, m;
         cin >> name >> r >> m;
-        Students obj(name, r, m);
-        cout << obj.name << endl;
+        Student obj(name, r, m);
+        pq.push(obj);
+    }
+
+    while (!pq.empty())
+    {
+
+        cout << pq.top().name << " " << pq.top().roll << " " << pq.top().mark << endl;
+        pq.pop();
     }
     return 0;
 }
