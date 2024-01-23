aa35a791578def70c6f820ee6a2d72b33ebd291d
Sujoy Paul <freelancersujoy@gmail.com>
2024-01-23 23:11:06 +0600

    🌼 STL Pair

---
+++ Semister 2/Algorithm/Module 1 Graph & Represents/STL_Pair.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // make pair with two diffrent diffrent data type and simillar also.
+    pair<string, int> p;
+    p = make_pair("Sujoy", 20);
+    cout << p.first << " " << p.second;
+
+    return 0;
+}
