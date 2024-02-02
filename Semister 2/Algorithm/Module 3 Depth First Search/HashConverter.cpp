08960490be4a300fa007b406351f70939857ccd9
Sujoy Paul <freelancersujoy@gmail.com>
2024-02-03 01:06:09 +0600

    DFS and BFS

---
+++ Semister 2/Algorithm/Module 3 Depth First Search/HashConverter.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    getline(cin, name);
+
+    for (int i = 0; i < name.size(); i++)
+    {
+        switch (name[i])
+        {
+        case 'a':
+            cout << 65;
+            break;
+        case 'A':
+            cout << 65;
+            break;
+        case 'b':
+            cout << 66;
+            break;
+        case 'B':
+            cout << 66;
+            break;
+        case 'c':
+            cout << 67;
+            break;
+        case 'C':
+            cout << 67;
+            break;
+        case 'd':
+            cout << 68;
+            break;
+        case 'D':
+            cout << 68;
+            break;
+        case 'e':
+            cout << 69;
+            break;
+        case 'E':
+            cout << 69;
+            break;
+        case 'f':
+            cout << 'k';
+            break;
+        case 'F':
+            cout << 'k';
+            break;
+        case 'g':
+            cout << 70;
+            break;
+        case 'G':
+            cout << 70;
+            break;
+        case 'h':
+            cout << 't';
+            break;
+        case 'H':
+            cout << 't';
+            break;
+        case 'i':
+            cout << 72;
+            break;
+        case 'I':
+            cout << 72;
+            break;
+        case 'j':
+            cout << 73;
+            break;
+        case 'J':
+            cout << 73;
+            break;
+        case 'k':
+            cout << 74;
+            break;
+        case 'K':
+            cout << 74;
+            break;
+        case 'l':
+            cout << 75;
+            break;
+        case 'L':
+            cout << 75;
+            break;
+        case 'm':
+            cout << 76;
+            break;
+        case 'M':
+            cout << 76;
+            break;
+        case 'n':
+            cout << 77;
+            break;
+        case 'N':
+            cout << 77;
+            break;
+        case 'o':
+            cout << 78;
+            break;
+        case 'O':
+            cout << 78;
+            break;
+        case 'p':
+            cout << 79;
+            break;
+        case 'P':
+            cout << 79;
+            break;
+        case 'q':
+            cout << 80;
+            break;
+        case 'Q':
+            cout << 80;
+            break;
+        case 'r':
+            cout << 81;
+            break;
+        case 'R':
+            cout << 81;
+            break;
+        case 's':
+            cout << 82;
+            break;
+        case 'S':
+            cout << 82;
+            break;
+        case 't':
+            cout << 83;
+            break;
+        case 'T':
+            cout << 83;
+            break;
+        case 'u':
+            cout << 84;
+            break;
+        case 'U':
+            cout << 84;
+            break;
+        case 'v':
+            cout << 85;
+            break;
+        case 'V':
+            cout << 85;
+            break;
+        case 'w':
+            cout << 86;
+            break;
+        case 'W':
+            cout << 86;
+            break;
+        case 'x':
+            cout << 87;
+            break;
+        case 'X':
+            cout << 87;
+            break;
+        case 'y':
+            cout << 88;
+            break;
+        case 'Y':
+            cout << 88;
+            break;
+        case 'z':
+            cout << 89;
+            break;
+        case 'Z':
+            cout << 89;
+            break;
+        default:
+            cout << '%';
+            break;
+        }
+    }
+    return 0;
+}
