91a19f3e531470c2357baff04d8f9f73582d3ce6
Sujoy Paul <freelancersujoy@gmail.com>
2024-02-05 11:34:16 +0600

    problem 1

---
+++ Semister 2/Algorithm/Assignment 1 Module 4/Connected_or_Not.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 5;
+vector<int> v[N];
+
+void dfs(int src, int cnc)
+{
+    bool flag = false;
+    for (int child : v[src])
+    {
+        if (child == cnc)
+        {
+            flag = true;
+            break;
+        }
+    };
+    if (flag == true || src == cnc)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        dfs(a, b);
+    }
+    return 0;
+}
