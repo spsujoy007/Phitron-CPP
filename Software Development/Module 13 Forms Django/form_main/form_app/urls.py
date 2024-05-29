d690e3de85f9851eb33052ee28be2ddd2723eb41
Sujoy Paul <freelancersujoy@gmail.com>
2024-05-29 10:16:53 +0600

    🫕 Module 13 done

---
+++ Software Development/Module 13 Forms Django/form_main/form_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('about/', views.home, name='about'),
+    path('form/', views.formpage, name='form')
+    # path('app/', include('form_app.urls'))
+]
