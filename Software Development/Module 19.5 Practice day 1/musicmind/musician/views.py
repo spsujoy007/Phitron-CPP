7a345266a623d42a5b86b6d0918e8506a1e5db37
Sujoy Paul <freelancersujoy@gmail.com>
2024-06-21 13:09:27 +0600

    🥢 Practice day 1

--- Software Development/Module 19.5 Practice day 1/musicmind/musician/views.py
+++ Software Development/Module 19.5 Practice day 1/musicmind/musician/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import MusicianForm
 from django.views.generic import CreateView
 from . import models
 from . import forms
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # def addMusician(req):
 #     form = MusicianForm()
 #     return render(req, 'add_musician.html', {'form': form})
 
+# @login_required
 class AddMusician(CreateView):
     model = models.Musician
     form_class = MusicianForm
     template_name = 'add_musician.html'
     success_url = '/'
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
     def form_valid(self, form):
         return super().form_valid(form)
